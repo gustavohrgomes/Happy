844c6112e3b6039bb2e4de22965817fe541b541f
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-13 23:32:30 -0300

    Add validation

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 import Orphanage from '../models/Orphanage';
 
@@ -44,8 +45,8 @@ export default {
     const images = requestImages.map(image => {
       return { path: image.filename }
     })
-  
-    const orphanage = orphanagesRepository.create({
+
+    const orphanageInfo = {
       name, 
       latitude,
       longitude,
@@ -54,7 +55,27 @@ export default {
       opening_hours,
       open_on_weekends,
       images
-    });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+      }))
+    })
+  
+    await schema.validate(orphanageInfo, {
+      abortEarly: false,
+    })
+
+    const orphanage = orphanagesRepository.create(orphanageInfo);
   
     await orphanagesRepository.save(orphanage);
 

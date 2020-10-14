c682f9681835ffe94cae33b760a41c8cd9b5d710
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-13 21:30:28 -0300

    Create Index & Show routes

--- backend/routes.ts
+++ backend/routes.ts
@@ -3,6 +3,8 @@ import OrphanageController from './src/controllers/OrphanagesController';
 
 const routes = Router()
 
+routes.get('/orphanages', OrphanageController.index);
+routes.get('/orphanages/:id', OrphanageController.show);
 routes.post('/orphanages', OrphanageController.create);
 
 export default routes;

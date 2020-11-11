01d37671341af48e9f6efd85dd27c0945b9d85be
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-11-11 00:49:45 -0300

    Conect with Happy Dotnet Core web api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  //baseURL: 'http://localhost:3333' Node web api
+  baseURL: 'https://localhost:5001', // Dotnet Core web api
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
